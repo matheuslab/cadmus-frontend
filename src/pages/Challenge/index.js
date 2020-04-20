89194a2f5df2113ab881371cd9e9b470be74c07d
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-04-20 20:59:09 -0300

    Rendering raw form on screen

--- src/pages/Challenge/index.js
+++ src/pages/Challenge/index.js
@@ -1,16 +1,84 @@
-import React from 'react';
+/* eslint-disable react/forbid-prop-types */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage, injectIntl } from 'react-intl';
 import Typography from '@material-ui/core/Typography';
-import { FormattedMessage } from 'react-intl';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 import messages from './messages';
+import { handleOnChange } from './handlers';
 
-export const Challenge = () => (
-  <Box>
-    <Typography>
-      <FormattedMessage {...messages.title} />
-    </Typography>
-  </Box>
-);
+export const Challenge = ({ intl }) => {
+  const [cloud, setCloud] = useState(4);
+  const [airport, setAirport] = useState(3);
+  const [width, setWidth] = useState(10);
+  const [height, setHeight] = useState(10);
 
-export default Challenge;
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="50%"
+      my={2}
+    >
+      <TextField
+        type="number"
+        value={cloud}
+        onChange={handleOnChange(setCloud)}
+        label={intl.formatMessage(messages.cloud)}
+        placeholder={intl.formatMessage(messages.cloudNumber)}
+        inputProps={{
+          min: 4,
+        }}
+      />
+
+      <TextField
+        type="number"
+        value={airport}
+        onChange={handleOnChange(setAirport)}
+        label={intl.formatMessage(messages.airport)}
+        placeholder={intl.formatMessage(messages.airportNumber)}
+        inputProps={{
+          min: 3,
+        }}
+      />
+
+      <TextField
+        type="number"
+        value={height}
+        onChange={handleOnChange(setHeight)}
+        label={intl.formatMessage(messages.height)}
+        placeholder={intl.formatMessage(messages.heightNumber)}
+        inputProps={{
+          min: 10,
+        }}
+      />
+
+      <TextField
+        type="number"
+        value={width}
+        onChange={handleOnChange(setWidth)}
+        label={intl.formatMessage(messages.width)}
+        placeholder={intl.formatMessage(messages.widthNumber)}
+        inputProps={{
+          min: 10,
+        }}
+      />
+
+
+      <Button onClick={handleOnChange(setCloud)}>
+        <Typography>
+          <FormattedMessage {...messages.calc} />
+        </Typography>
+      </Button>
+    </Box>
+  );
+};
+
+Challenge.propTypes = {
+  intl: PropTypes.object.isRequired,
+};
+
+export default injectIntl(Challenge);
--- src/pages/Challenge/messages.js
+++ src/pages/Challenge/messages.js
@@ -7,4 +7,40 @@ export default defineMessages({
     id: `${scope}.title`,
     defaultMessage: 'Hello World',
   },
+  cloud: {
+    id: `${scope}.cloud`,
+    defaultMessage: 'Nuvens',
+  },
+  cloudNumber: {
+    id: `${scope}.cloudNumber`,
+    defaultMessage: 'Número de Nuvens (mínimo 4)',
+  },
+  airport: {
+    id: `${scope}.airport`,
+    defaultMessage: 'Aeroportos',
+  },
+  airportNumber: {
+    id: `${scope}.airportNumber`,
+    defaultMessage: 'Número de Aeroportos (mínimo 3)',
+  },
+  height: {
+    id: `${scope}.height`,
+    defaultMessage: 'Altura',
+  },
+  heightNumber: {
+    id: `${scope}.heightNumber`,
+    defaultMessage: 'Tamanho na vertical',
+  },
+  width: {
+    id: `${scope}.width`,
+    defaultMessage: 'Comprimento',
+  },
+  widthNumber: {
+    id: `${scope}.widthNumber`,
+    defaultMessage: 'Tamanho na horizontal',
+  },
+  calc: {
+    id: `${scope}.calc`,
+    defaultMessage: 'Calcular',
+  },
 });
