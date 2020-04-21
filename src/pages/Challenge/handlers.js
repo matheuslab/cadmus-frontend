7a7d6e10257cc1ee54a6e6ee23de01a60e0e8b70
Matheus Martins <matheus.martins@ee.ufcg.edu.br>
2020-04-20 21:12:44 -0300

    post initial data to api

--- src/pages/Challenge/handlers.js
+++ src/pages/Challenge/handlers.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-alert */
+import api from '../../services/api';
+
 export const handleOnChange = (setState) => (event) => {
   setState(event.target.value);
+};
+
+export const handleOnSubmit = (data) => async (event) => {
+  event.preventDefault();
+
+  try {
+    await api.post('challenge', data);
+
+    alert('Dados enviados com sucesso');
+  } catch (err) {
+    alert('Erro no envio dos dados, tente novamente.');
+  }
 };
